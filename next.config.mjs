e3fb6a40209fb1b8672be68843f48a03c1550916
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2025-01-11 22:00:36 -0300

    teste

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
+    console.log(config); // Exibe a configuração no terminal
+    return config;
+  },
 };
 
 export default nextConfig;
